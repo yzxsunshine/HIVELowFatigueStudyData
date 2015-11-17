aa1229709d07ef23f32012a8751f25f9182a9f03
Zhixin Yan <zyan@wpi.edu>
2015-11-17 10:45:15 -0500

    subject 12 - 16

---
+++ ParsingUserStudyData/ParsingUserStudyData/Quaternion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingUserStudyData
+{
+    class Quaternion
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+    };
+}
---
+++ ParsingUserStudyData/ParsingUserStudyData/SubjectData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingUserStudyData
+{
+    class WalkingData {
+
+    };
+
+    class SegwayData {
+
+    };
+
+    class SurfingData {
+
+    };
+
+    class ModeType <DataType> {
+        List<DataType> trialData;
+        List<DataType> trialData;
+    };
+
+    class SubjectData
+    {
+        int id;
+        float 
+    }
+}
